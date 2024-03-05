4a017ac23f0982a5646e8f7200386603ec094c02
Siddharth Dagar <sid.dagar0101@gmail.com>
2024-03-05 18:27:11 +0530

    replace blanks

--- index.py
+++ index.py
@@ -3,20 +3,22 @@ word_list = ["siddharth", "gandharva", "lakshay"]
 import random
 
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 print(f'the chosen word is {chosen_word}')
 
 display = []
-for _ in range(len(chosen_word)):
+for _ in range(word_length):
     display += "_"
 
 print(display)   
- 
+
 
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")    
+        display[position] = guess
+
+print(display)
